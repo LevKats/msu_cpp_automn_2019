a232ac87afbe3df7900e022930a26bb78db3c112
Leva <lev-katz@mail.com>
2019-10-10 17:01:01 +0300

    fixed extra tabs

--- homework/00/vertical.cpp
+++ homework/00/vertical.cpp
@@ -1,18 +1,22 @@
 #include "timer.h"
 
 int main() {
-	const size_t HSIZE = 10 * 1000;
-	const size_t WSIZE = 10 * 1000;
-	unsigned int **mas = new unsigned int*[HSIZE];
-	for (int i = 0; i < HSIZE; ++i)
-		mas[i] = new unsigned int[WSIZE];
-	unsigned int sum = 0;
-	Timer t;
-	for (int j = 0; j < WSIZE; ++j)	
-		for (int i = 0; i < HSIZE; ++i) {
-			volatile unsigned int a = mas[i][j];
-			sum += a;
-		}
-	return 0;
+    const size_t HSIZE = 10 * 1000;
+    const size_t WSIZE = 10 * 1000;
+    unsigned int **mas = new unsigned int*[HSIZE];
+    for (int i = 0; i < HSIZE; ++i)
+        mas[i] = new unsigned int[WSIZE];
+    unsigned int sum = 0;
+    Timer *t = new Timer();
+    for (int j = 0; j < WSIZE; ++j)    
+        for (int i = 0; i < HSIZE; ++i) {
+            volatile unsigned int a = mas[i][j];
+            sum += a;
+        }
+    delete t;
+    for (int i = 0; i < HSIZE; ++i)
+        delete[] mas[i];
+    delete[] mas;
+    return 0;
 }
 
