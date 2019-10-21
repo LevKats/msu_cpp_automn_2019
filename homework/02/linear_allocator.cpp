dc68f6c8f0ed4a11498d01808f36cdcd8864e612
Leva <lev-katz@mail.ru>
2019-10-21 16:56:16 +0300

    this-> removed, bad_alloc fixed

--- homework/02/linear_allocator.cpp
+++ homework/02/linear_allocator.cpp
@@ -2,32 +2,26 @@
 #include <new>
 #include <iostream>
 
-LinearAllocator::LinearAllocator(std::size_t size) : size(size) {
-    try {
-        this->buf = new char[size];
-    }
-    catch (std::bad_alloc &ba) {
-	    // std::cerr << "can't allocate memory " << ba.what() << '\n';
-        throw std::out_of_range("can't allocate memory");
-    }
-    //todo throw something
-    this->pos = this->buf;
+LinearAllocator::LinearAllocator(std::size_t size) : size(size)
+{ 
+    buf = new char[size];
+    pos = buf;
 }
 
 LinearAllocator::~LinearAllocator() {
-    delete[] this->buf;
+    delete[] buf;
 }
 
-char* LinearAllocator::alloc(std::size_t size) {
-    if (size == 0) { return nullptr; }
-    if (this->size - (this->pos - this->buf) < size)
+char* LinearAllocator::alloc(std::size_t request_size) {
+    if (request_size == 0) { return nullptr; }
+    if (size - (pos - buf) < request_size)
         return nullptr;
-    char* result = this->pos;
-    this->pos += size;
+    char* result = pos;
+    pos += request_size;
     return result;
 }
 
 void LinearAllocator::reset() {
-    this->pos = this->buf;
+    pos = buf;
 }
 
